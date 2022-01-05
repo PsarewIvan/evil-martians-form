40f7bbea8a5a2d55b749597392fbff506acd67c3
Ivan Psarew <psarew.ivan@yandex.ru>
2022-01-05 23:20:45 +0300

    remove unused dependencies

--- src/components/FormCreateAcc/FormCreateAcc.tsx
+++ src/components/FormCreateAcc/FormCreateAcc.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import useFormField from '../../hooks/useFormField';
 import FormInput from '../FormInput/FormInput';
