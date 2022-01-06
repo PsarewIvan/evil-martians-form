fe1f94e6cf753e4c2752e3c8490f0c8a86204e7c
Ivan Psarew <psarew.ivan@yandex.ru>
2022-01-07 01:47:06 +0300

    add button disabled state

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,13 +4,14 @@ import { ButtonProps } from './types';
 import s from './Button.module.scss';
 
 const Button = (props: ButtonProps): JSX.Element => {
-  const { children, onClick, submit } = props;
+  const { children, onClick, submit, disabled = false } = props;
 
   return (
     <button
       className={s.button}
       onClick={onClick}
       type={submit ? 'submit' : 'button'}
+      disabled={disabled}
     >
       {children}
     </button>
